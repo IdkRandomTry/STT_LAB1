4d8554bdf3ec16cd41b191534f7e540a7a3e0283
IdkRandomTry <umarjeesiddhesh@gmail.com>
2025-09-05 11:40:56 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import os,sys
+
+x = 10
+y=20  # bad spacing
+
+def add(a,b): 
+  return a+b    # bad indentation
+
+def Unused(foo, bar):  # unused args 
+    temp = 42   # unused variable
+    return None
+
+def main():
+    z = add(x,y)
+    print("Result is " + str(z))  # ok
+    if True == True:   # comparison with constant
+        print("Always true")
+    for i in range(5):
+        print("hello world") # unused loop variable i
+main() 
